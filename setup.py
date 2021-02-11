e547f606166cf98cc1cf94b03e9bc57c8078d619
IonLing <soarvision@outlook.com>
2021-02-11 19:48:28 +0800

    📦 (Flash) Change to setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+import setuptools
+
+
+setuptools.setup(
+    name="flash",
+    version="0.0.2",
+    packages=["."],
+    install_requires=["click==7.1.2", "toml==0.10.2"],
+    entry_points={"console_scripts": ["flash=flash:cli"]},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
