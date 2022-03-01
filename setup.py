d017cbf7f859240ea35d82069d766d32a7c92fcc
ionling <soarvision@outlook.com>
2022-03-01 21:01:29 +0800

    ⬆️ Upgrade click to >=8.0.0

--- setup.py
+++ setup.py
@@ -3,9 +3,9 @@ import setuptools
 
 setuptools.setup(
     name="flash",
-    version="0.1.1",
+    version="0.2.0",
     packages=["."],
-    install_requires=["click==7.1.2", "toml==0.10.2"],
+    install_requires=["click>=8.0.0", "toml==0.10.2"],
     entry_points={"console_scripts": ["flash=flash:cli"]},
     classifiers=[
         "Programming Language :: Python :: 3",
